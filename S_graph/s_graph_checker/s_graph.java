fd464e18f4559408389b2ef761ea896a0e835121
Le Zhang <zhrobinson@gmail.com>
2017-10-03 15:42:12 -0500

    create s_graph.java

---
+++ S_graph/s_graph_checker/s_graph.java
@@ -0,0 +1,72 @@
+package s_graph;
+
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.ensoftcorp.atlas.core.db.graph.Edge;
+import com.ensoftcorp.atlas.core.db.graph.Node;
+import com.ensoftcorp.atlas.core.query.Q;
+import com.ensoftcorp.atlas.core.script.Common;
+import com.ensoftcorp.atlas.core.xcsg.XCSG;
+import com.ensoftcorp.open.c.commons.analysis.CommonQueries;
+
+public class s_graph {
+	public static boolean fun_isSgraph(String fun) {
+		Q cfg=CommonQueries.cfg(CommonQueries.functions(fun)); //Control flow graph
+		Q cfbe=cfg.edges(XCSG.ControlFlowBackEdge); //Control flow back edge
+		Q dag=cfg.differenceEdges(cfbe); //Directed Acyclic Graph (DFBE removed)
+		
+		
+		
+		// record all nodes that connect to CFBE
+		Set<Node> legal_exit_nodes = new HashSet<Node>();
+		for(Edge e : cfbe.eval().edges()) {
+			for(Node n : cfg.predecessorsOn(Common.toQ(e)).eval().nodes()) {
+				legal_exit_nodes.add(n);
+			}
+		}
+		for(Node n : cfg.leaves().eval().nodes()) {
+			legal_exit_nodes.add(n);
+		}
+		
+		// check if S-graph
+		for(Node n : dag.eval().nodes()) {
+			if(dag.forward(Common.toQ(n)).eval().roots().size()!=1) {
+				return false;
+			}
+			for(Node p : dag.forward(Common.toQ(n)).leaves().eval().nodes()) {
+				if(!legal_exit_nodes.contains(p)) {
+					return false;
+				}
+			}
+
+		}	
+		return true;
+	}
+	
+	public static void proj_isSgraph(String proj) {
+		Q classes=Common.universe().project(proj).contained().nodes(XCSG.Function);
+		String name="";
+		String res= "";
+		boolean status;
+		for(Node n:classes.eval().nodes()) {
+			name = n.getAttr(XCSG.name).toString();
+			status = fun_isSgraph(name);
+			if(!status)
+				res += name+","+(status? "true":"false")+"\n";
+			
+		}
+		if(res=="")
+			res+="None";
+		
+		try {
+			PrintWriter writer = new PrintWriter("/home/leo/test-Sgraph-"+proj+".csv", "UTF-8");
+			writer.print(res);
+			writer.close();
+		}catch(Exception e) {
+			
+		}
+	}
+	
+}
